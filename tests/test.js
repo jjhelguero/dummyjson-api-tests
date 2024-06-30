0ab7014ea3f8a07a1f1e66adfcb48644bf15e5f2
jjhelguero <56001598+jjhelguero@users.noreply.github.com>
2024-06-30 17:35:02 -0500

    update caseing for env var

--- tests/test.js
+++ tests/test.js
@@ -3,8 +3,8 @@ const { spec, request } = require("pactum");
 request.setBaseUrl("https://dummyjson.com");
 
 it("should get a response with status code 200", async () => {
-  const username = process.env.admin_username;
-  const password = process.env.admin_password;
+  const username = process.env.ADMIN_USERNAME;
+  const password = process.env.ADMIN_PASSWORD;
 
   await spec()
     .post("/auth/login")
